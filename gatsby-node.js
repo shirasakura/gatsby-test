70d13a974d24e931859c5a32ed6566c7f9239300
shirasakura <mshr0520@icloud.com>
2021-02-10 00:06:40 +0900

    second commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,46 +1,42 @@
-const fs = require('fs')
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const Promise = require('bluebird')
+const path = require('path')
 
-exports.createPages = async ({ graphql, actions }) => {
+exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  const result = await graphql(
-    `
-      {
-        allContentfulBlogPost( 
-          sort: { fields: [date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              title2
-              image
-              slug
-              body
-              publishDate
+  return new Promise((resolve, reject) => {
+    const blogPost = path.resolve('./src/templates/blog-post.js')
+    resolve(
+      graphql(
+        `
+          {
+            allContentfulBlogPost {
+              edges {
+                node {
+                  title
+                  slug
+                }
+              }
             }
           }
+          `
+      ).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
         }
-      }
-    `
-  )
 
-  if (result.errors) {
-    throw result.errors
-  }
-
-  // Create blog posts pages.
-  const posts = result.data.allContentfulBlogPost.edges
-
-  posts.forEach((post, index) => {
-    createPage({
-      path: post.node.slug,
-      component: blogPost,
-      context: {
-        slug: post.node.slug
-      },
-    })
+        const posts = result.data.allContentfulBlogPost.edges
+        posts.forEach((post, index) => {
+          createPage({
+            path: `/blog/${post.node.slug}/`,
+            component: blogPost,
+            context: {
+              slug: post.node.slug
+            },
+          })
+        })
+      })
+    )
   })
 }
