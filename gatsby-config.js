476d41a52292ee742e4d421722f3a195138dc06f
shirasakura <mshr0520@icloud.com>
2021-02-09 23:32:36 +0900

    second commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,10 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
-        accessToken: process.env.GATSBY_CONTENTFUL_API_KEY,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
         host: process.env.CONTENTFUL_HOST || `cdn.contentful.com`,
+        environment: 'master',
       }
     }
   ],
